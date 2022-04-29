7f4ea80adc53068030d733a44ac7506bdd22828e
God'spower <westcode6@gmail.com>
2022-04-30 00:11:33 +0100

    new commit

--- public/index.js
+++ public/index.js
@@ -1,10 +1,21 @@
 
 let overlayEl = document.getElementById("overlay-el");
+let sidebarEl = document.getElementById("sidebar-btn");
 function showSideNav() {
 overlayEl.classList.toggle("hidden")
+
+sidebarEl.textContent = "x";
+
 }
 
 function closeSideNav() {
 overlayEl.classList.toggle("hidden")
+sidebarEl.textContent = "=";
+
+
+}
+
+function navFunc() {
+sidebarEl.textContent = "=";
 
 }
