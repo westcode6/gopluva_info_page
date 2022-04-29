812a27993aaa943c27fbdb1fd8894a01db999e7e
God'spower <westcode6@gmail.com>
2022-04-29 23:39:59 +0100

    more

---
+++ public/index.js
@@ -0,0 +1,10 @@
+
+let overlayEl = document.getElementById("overlay-el");
+function showSideNav() {
+overlayEl.classList.toggle("hidden")
+}
+
+function closeSideNav() {
+overlayEl.classList.toggle("hidden")
+
+}
